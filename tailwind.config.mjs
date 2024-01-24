9a26a1e1cd1f6297218e2cf41d38ffe13590dcf9
didier lugo <didierlugo4@gmail.com>
2024-01-24 17:43:26 -0500

    se modificaron elementos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily:{
-				Rubik:[ 'Rubik', 'sans-serif'],
-				Oswald:['Oswald', 'sans-serif']
-
-			}
-			
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Rubik: ["Rubik", "sans-serif"],
+        Oswald: ["Oswald", "sans-serif"],
+      },
+      colors: {
+        
+		'royal-blue': {
+			'50': '#eff4fe',
+			'100': '#e1ecfe',
+			'200': '#c9d9fc',
+			'300': '#a9c0f8',
+			'400': '#869df3',
+			'500': '#697ceb',
+			'600': '#5b64e1',
+			'700': '#3e45c4',
+			'800': '#353c9e',
+			'900': '#31387e',
+			'950': '#1d1f49',
 		},
-	},
-	plugins: [],
-}
+		
+		
+		
+        
+      },
+    },
+  },
+  plugins: [],
+};
